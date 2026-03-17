7215a3032b26aa38a6829dc253c61cd19ec719c2
BenjaminTzoc00 <jtzoc@universales.com>
2026-03-16 23:50:38 -0600

    feat: Add sales module services for customer, payment, order, and quotation management, and configure environments.

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://web-i90ouh5prcr8.up-de-fra1-k8s-1.apps.run-on-seenode.com',
+    apiUrl: 'https://web-i90ouh5prcr8.up-de-fra1-k8s-1.apps.run-on-seenode.com/api/v1',
+}
